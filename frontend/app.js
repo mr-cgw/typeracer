74bc4e965e6a0c9a2e7933c98c929e7743021882
Yizhe Wang <yizhe87@gmail.com>
2021-01-24 13:38:39 -0800

    battleship javascript codes

---
+++ frontend/app.js
@@ -0,0 +1,149 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const userGrid = document.querySelector('.grid-user');
+  const computerGrid = document.querySelector('.grid-computer');
+  const displayGrid = document.querySelector('.grid-display');
+  const ships = document.querySelectorAll('.ship');
+  const destroyer = document.querySelector('.destroyer-container');
+  const submarine = document.querySelector('.submarine-container');
+  const cruiser = document.querySelector('.cruiser-container');
+  const battleship = document.querySelector('.battleship-container');
+  const carrier = document.querySelector('.carrier-container');
+  const startButton = document.querySelector('#start');
+  const rotateButton = document.querySelector('#rotate');
+  const turnDisplay = document.querySelector('#whose-go');
+  const infoDisplay = document.querySelector('#info');
+  const userSquares = [];
+  const computerSquares = [];
+  let isHorizontal = true;
+  const width = 10;
+
+  function createBoard(grid, squares) {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.dataset.id = i;
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard(userGrid, userSquares);
+  createBoard(computerGrid, computerSquares);
+
+  const shipArray = [
+    {
+      name: 'destroyer',
+      directions: [
+        [0, 1],
+        [0, width],
+      ],
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width * 2],
+      ],
+    },
+    {
+      name: 'battleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width * 2, width * 3],
+      ],
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width * 2, width * 3, width * 4],
+      ],
+    },
+  ];
+
+  function generate(ship) {
+    let randomDirection = Math.floor(Math.random() * ship.directions.length);
+    let current = ship.directions[randomDirection];
+    if (randomDirection === 0) direction = 1;
+    if (randomDirection === 1) direction = 10;
+    let randomStart = Math.abs(
+      Math.floor(Math.random() * computerSquares.length) -
+        ship.directions[0].length * direction
+    );
+    const isTaken = current.some((index) =>
+      computerSquares[randomStart + index].classList.contains('taken')
+    );
+    const isAtRightEdge = current.some(
+      (index) => (randomStart + index) % width === width - 1
+    );
+    const isAtLeftEdge = current.some((index) => (randomStart + index) % width === 0);
+    if (!isTaken && !isAtRightEdge && !isAtLeftEdge) {
+      current.forEach((index) =>
+        computerSquares[randomStart + index].classList.add('taken', ship.name)
+      );
+    } else {
+      generate(ship);
+    }
+  }
+  shipArray.forEach((ship) => generate(ship));
+  function rotate() {
+    // if (isHorizontal) {
+    destroyer.classList.toggle('destroyer-container-vertical');
+    submarine.classList.toggle('submarine-container-vertical');
+    cruiser.classList.toggle('cruiser-container-vertical');
+    battleship.classList.toggle('battleship-container-vertical');
+    carrier.classList.toggle('carrier-container-vertical');
+    isHorizontal = !isHorizontal;
+    // }
+  }
+  rotateButton.addEventListener('click', rotate);
+
+  let draggedShip;
+  let draggedShipLength;
+  let selectedShipNameWithIndex;
+
+  ships.forEach((ship) =>
+    ship.addEventListener('mousedown', (e) => {
+      selectedShipNameWithIndex = e.target.id;
+    })
+  );
+
+  function dragStart() {
+    draggedShip = this;
+    draggedShipLength = this.childNodes.length;
+    console.log('draggedShip', draggedShip);
+  }
+  function dragOver(e) {
+    e.preventDefault();
+  }
+  function dragEnter(e) {
+    e.preventDefault();
+  }
+  function dragLeave() {
+    console.log('dragleave');
+  }
+  function dragDrop() {
+    let shipNameWithLastId = draggedShip.lastElementChild.id;
+    console.log('lastId', shipNameWithLastId);
+    debugger;
+    let shipClass = shipNameWithLastId.slice(0, -2);
+    console.log('shipClass', shipClass);
+  }
+  function dragEnd() {
+    console.log('end');
+  }
+  ships.forEach((ship) => {
+    ship.addEventListener('dragstart', dragStart);
+  });
+  userSquares.forEach((square) => square.addEventListener('dragstart', dragStart));
+  userSquares.forEach((square) => square.addEventListener('dragover', dragOver));
+  userSquares.forEach((square) => square.addEventListener('dragenter', dragEnter));
+  userSquares.forEach((square) => square.addEventListener('dragleave', dragLeave));
+  userSquares.forEach((square) => square.addEventListener('dragdrop', dragDrop));
+  userSquares.forEach((square) => square.addEventListener('dragend', dragEnd));
+});
